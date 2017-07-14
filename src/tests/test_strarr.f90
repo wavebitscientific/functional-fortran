1ce5ae61c1a3231952e7b13a03577918aff4066a
Milan Curcic <caomaco@gmail.com>
2017-07-14 14:21:00 -0400

    add unit tests for arrstr and strarr

---
+++ src/tests/test_strarr.f90
@@ -0,0 +1,32 @@
+program test_strarr
+use mod_testing,only:assert,initialize_tests,report_tests
+use mod_functional,only:arrstr,strarr,empty
+
+implicit none
+
+logical,dimension(:),allocatable :: tests
+logical :: test_failed
+integer :: n,norder,ntests
+integer,parameter :: stdout = 6
+
+n = 1
+ntests = 3
+call initialize_tests(tests,ntests)
+
+tests(n) = assert(all(strarr('hello') == ['h','e','l','l','o']),&
+                  'strarr converts to array')
+n = n + 1
+
+tests(n) = assert(all(strarr('') == empty(' ')),&
+                  'strarr converts empty string to []')
+n = n + 1
+
+tests(n) = assert(arrstr(strarr('hello')) == 'hello',&
+                  'arrstr(strarr(string)) == string')
+n = n + 1
+
+test_failed = .false.
+call report_tests(tests,test_failed)
+if(test_failed)stop 1
+
+endprogram test_strarr
